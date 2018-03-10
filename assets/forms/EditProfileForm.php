aaa2ecfc76c5f265e6efef588bb402df18c8828a
Ben Waters <bjwaters@email.neit.edu>
2018-03-09 19:34:22 -0500

    Public and edit form changes

--- assets/forms/EditProfileForm.php
+++ assets/forms/EditProfileForm.php
@@ -1,4 +1,4 @@
- <?php
+<?php
 /**
  * Created by PhpStorm.
  * User: 001427082
@@ -9,19 +9,19 @@
 
 
 <div id = "editProfile" class="container border my-4 col-5">
-    <div class="row ">
+    <div class="flex-grow ">
         <form name = "editForm" method = 'post' action = "indexLog.php" enctype = "multipart/form-data">
 
-            <div class="border ml-3"><h3>Public Profile (Editing)</h3></div>
+            <div class="flex-row border ml-3"><h3>Public Profile (Editing)</h3></div>
 
-            <div class="row border ml-1" style="width:650px;">
+            <div class="d-flex flex-row border">
                 <div class="col border">
                     <img src = 'assets/Uploads/<?php echo $editPicture?>' width='200'>
                 </div>
 
-                <div class="col border float-right">
+                <div class="col border">
                     <input type="hidden" name="user_id" value="<?php echo $editUserID;?>"/> <br>
-                    Public Name: <input type="text" name="userName" value="<?php echo $editUserName;?>"/> <br>
+                    Name: <input type="text" name="userName" value="<?php echo $editUserName;?>"/> <br>
                     Location: <input type="text" name="city" value="<?php echo $editCity;?>" /><br>
 
                     State: <select name='state_drop'
@@ -39,39 +39,39 @@
                     </select>
 
                     <div = "little" <?php echo $hidden ?>>
-                        Genre:
-                        <select name='genre_drop'>
-                            <?php
-                            echo("<option selected='selected' value=null>Please choose a genre.</option>");
-                            $genreList = genreArray();
-                            foreach($genreList as $genre)
-                                if($editGenre == $genre)
-                                {
-                                    echo "<option selected = 'selected' value='" . $genre . "'>" . $genre . "</option>" . PHP_EOL;
-                                }
-                                else
-                                    echo "<option value='" . $genre . "'>" . $genre . "</option>" . PHP_EOL;
-                            ?>
-                        </select><br>
-                        Pay Rate: <input type="text" name="pay" value="<?php echo $editPay;?>"/><br>
-                        Availability: <input type="text" name="availability" value="<?php echo $editAvailability;?>"/><br>
-                    </div>
+                    Genre:
+                    <select name='genre_drop'>
+                        <?php
+                        echo("<option selected='selected' value=null>Please choose a genre.</option>");
+                        $genreList = genreArray();
+                        foreach($genreList as $genre)
+                            if($editGenre == $genre)
+                            {
+                                echo "<option selected = 'selected' value='" . $genre . "'>" . $genre . "</option>" . PHP_EOL;
+                            }
+                            else
+                                echo "<option value='" . $genre . "'>" . $genre . "</option>" . PHP_EOL;
+                        ?>
+                    </select><br>
+                    Pay Rate: <input type="text" name="pay" value="<?php echo $editPay;?>"/><br>
+                    Availability: <input type="text" name="availability" value="<?php echo $editAvailability;?>"/><br>
                 </div>
-
             </div>
 
-        <div class="row border ml-1" style="width:650px;">
-            <div class="col border">
-                Picture: <input type = 'file' name = 'file' id="file">
-            </div>
-            <div class="col border float-right">
-                Comments:<br> <textarea name="comments" rows="5" cols="40"> <?php echo $editComments;?> </textarea><br><br>
-            </div>
-        </div>
-
-            <br>
-            Video Link: <input type="text" name="videoLink" value="<?php echo $editVideoLink;?>"/><br><br>
-            <input type = "submit" name = "action" value = "Save Edit" />
-        </form>
     </div>
+
+    <div class="d-flex flex-row border">
+        <div class="col border" style="width:200px;">
+            Picture: <input type = 'file' name = 'file' id="file">
+        </div>
+        <div class="col border mr-auto">
+            Comments:<br> <textarea name="comments" rows="5" cols="40"> <?php echo $editComments;?> </textarea><br><br>
+        </div>
+    </div>
+
+    <br>
+    Video Link: <input type="text" name="videoLink" value="<?php echo $editVideoLink;?>"/><br><br>
+    <input type = "submit" name = "action" value = "Save Edit" />
+    </form>
+</div>
 </div>
